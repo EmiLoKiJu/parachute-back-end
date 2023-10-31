bb2ea41c97fe02e59bc8f6728a2d65842fcc29b8
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-10-31 10:10:30 -0600

    Add localhost 5173 as an allowed origin for API calls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5000'
+    origins 'localhost:5173'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post]
+      methods: [:get, :post, :delete]
   end
 end
